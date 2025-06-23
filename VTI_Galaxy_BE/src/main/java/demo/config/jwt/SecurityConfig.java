077b27ad6776637103bda18b0a325ffd284c8036
Trần Văn Lâm <132673081+tzanlam@users.noreply.github.com>
2025-06-23 10:39:57 +0700

    fix security

--- VTI_Galaxy_BE/src/main/java/demo/config/jwt/SecurityConfig.java
+++ VTI_Galaxy_BE/src/main/java/demo/config/jwt/SecurityConfig.java
@@ -61,6 +61,7 @@ public class SecurityConfig {
                 .sessionManagement(i->i.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(a->a
                         .requestMatchers(
+                                "/getRooms",
                                 "/registerUser",
                                 "/postMovie",
                                 "/login",
