8fb5a4726d41f0024ba8151840a9ca80ba85c10d
tzanlam <tzanlam@gmail.com>
2025-08-07 19:38:07 +0700

    fix change name database and add seeder

--- VTI_Galaxy_BE/src/main/java/demo/repository/OtherRepository.java
+++ VTI_Galaxy_BE/src/main/java/demo/repository/OtherRepository.java
@@ -4,10 +4,6 @@ import demo.modal.entity.Other;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface OtherRepository extends JpaRepository<Other, Integer> {
-    List<Other> findByGalaxyIdAndStatus(Integer galaxyId, String status);
-
 }
--- VTI_Galaxy_BE/src/main/java/demo/repository/RoomRepository.java
+++ VTI_Galaxy_BE/src/main/java/demo/repository/RoomRepository.java
@@ -4,6 +4,9 @@ import demo.modal.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Integer> {
+    List<Room> findByGalaxyId(Integer galaxyId);
 }
