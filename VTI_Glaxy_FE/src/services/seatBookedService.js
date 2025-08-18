42ba1cd7fdee6058ca2af9716e0231432be44f37
hoang12343 <hoanggg322004@gmail.com>
2025-08-18 18:12:52 +0700

    code

--- VTI_Glaxy_FE/src/services/seatBookedService.js
+++
@@ -1,8 +0,0 @@
-import axiosClient from "./axiosClient"
-
-const seatBookedService = {
-    fetchSeatBooked(roomId, time){
-        return axiosClient.get(`/roomId/${roomId}/time/${time}`)
-    }
-}
-export default seatBookedService
